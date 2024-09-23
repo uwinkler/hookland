b6bc374deb7a4e476cd36288f842929c935d3de1
uli.winkler <uli.winkler@gmail.com>
2024-09-23 04:39:14 +0200

    Inject: Fixed linter erros and add watch:test script

--- packages/inject/vite.config.ts
+++ packages/inject/vite.config.ts
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite' // Import the 'resolve' function
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
