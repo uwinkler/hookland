7ad5d7ca15f902e52e1c5f3d1ac2aa849579fc8e
uli.winkler <uli.winkler@gmail.com>
2024-09-23 04:37:02 +0200

    Fix build
    - using tsc vor building the lib
    - fixed CI build

--- packages/inject/vite.config.ts
+++ packages/inject/vite.config.ts
@@ -1,12 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite' // Import the 'resolve' function
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: './src/index.ts',
+      entry: './src',
+      name: 'hookland-inject',
       formats: ['es']
     }
   }
