a7a47d3822dc461f41ac223a984250ff94e23369
uli.winkler <uli.winkler@gmail.com>
2025-02-16 08:55:58 +0100

    clean init

--- packages/dev-tools-chrome-old/src/components/useState.tsx
+++
@@ -1,5 +0,0 @@
-import { State } from '../model/state.model'
-
-export const defaultState: State = {
-  uiConfigs: []
-}
--- packages/dev-tools-chrome-old/src/model/state.model.tsx
+++
@@ -1,5 +0,0 @@
-import type { UiConfig } from '@hookland/dev-tools-commons'
-
-export type State = {
-  uiConfigs: UiConfig[]
-}
